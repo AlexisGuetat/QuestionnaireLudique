bc8b9a416890ee06b2a936893d34c7328def114f
Alex <43337688+AlexisGuetat@users.noreply.github.com>
2019-01-25 14:49:40 +0100

    ajout du classement

--- src/ATC/AppBundle/Controller/ClassementController.php
+++ src/ATC/AppBundle/Controller/ClassementController.php
@@ -2,12 +2,20 @@
 
 namespace ATC\AppBundle\Controller;
 
+
+use  ATC\AppBundle\Entity\Joueur;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ClassementController extends Controller
 {
     public function indexAction()
-    {
-        return $this->render('ATCAppBundle:Classement:index.html.twig');
+    {   
+        $bdd = $this->getDoctrine()->getManager();
+
+        $joueurs =  $bdd->getRepository('ATCAppBundle:Joueur')->findAll();
+
+        return $this->render('ATCAppBundle:Classement:index.html.twig', array(
+            'joueurs' => $joueurs
+        ));
     }
 }
