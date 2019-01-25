645effcc3e25fc99cebbfe87d8475c74b98beb8a
Alex <43337688+AlexisGuetat@users.noreply.github.com>
2019-01-25 13:11:20 +0100

    jout de questionnaireController

---
+++ src/ATC/AppBundle/Controller/QuestionController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ATC\AppBundle\Controller;
+
+use  ATC\AppBundle\Entity\Question;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class QuestionController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $pseudo = $request->query->get('pseudo'); 
+        return $this->render('ATCAppBundle:Questionnaire:add.html.twig',array('pseudo'=>$pseudo));
+    }
+
+    public function facileAction(Request $request)
+    {
+        $pseudo = $request->query->get('pseudo'); 
+        return $this->render('ATCAppBundle:Questionnaire:Difficulte/facile.html.twig',array('pseudo'=>$pseudo));
+    }
+
+    public function moyenAction(Request $request)
+    {
+        $pseudo = $request->query->get('pseudo'); 
+        return $this->render('ATCAppBundle:Questionnaire:Difficulte/moyen.html.twig',array('pseudo'=>$pseudo));
+    }
+
+    public function difficileAction(Request $request)
+    {
+        $pseudo = $request->query->get('pseudo'); 
+        return $this->render('ATCAppBundle:Questionnaire:Difficulte/difficile.html.twig',array('pseudo'=>$pseudo));
+    }
+
+    public function mathAction(Request $request)
+    {
+        $pseudo = $request->query->get('pseudo'); 
+        return $this->render('ATCAppBundle:Questionnaire:Theme/math.html.twig',array('pseudo'=>$pseudo));
+    }
+
+
+}
