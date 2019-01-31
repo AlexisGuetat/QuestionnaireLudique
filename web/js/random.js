0688792b76fa7ab997a023e1415b22006dc425ed
theoLP <caltot/theo27@gmail.com>
2019-01-31 09:33:25 +0100

    pb fonctionnement JS fonction random

--- web/js/random.js
+++ web/js/random.js
@@ -1,15 +1,21 @@
-var elems = [], tempElem, pn, args = orderElems.arguments;
- for( var i = 0, len = args.length; i < len; i++ )
-  ( pn = ( elems[ i ] = tempElem = document.getElementById( args[ i ] ) ).parentNode).removeChild( tempElem );
-  
- for( var i = len - 1, idx, tmp; i > -1; i--)
- { 
-  while( ( idx = Math.floor( Math.random() * len ) ) == i );
-  tmp = elems[ i ];
-  elems[ i ] = elems[ idx ];
-  elems[ idx ] = tmp;
- } 
- for( var i = 0; i < len; i++ )
-  pn.appendChild( elems[ i ] );
+  /*function orderElems()
+    {
+      var elems = [], tempElem, pn, args = orderElems.arguments;
+                 
+      for( var i = 0, len = args.length; i < len; i++ )
+        ( pn = ( elems[ i ] = tempElem = document.getElementById( args[ i ] ) ).parentNode).removeChild( tempElem );
+        for( var i = len - 1, idx, tmp; i > -1; i-- ){ 
+          while(( idx = Math.floor( Math.random() * len )) == i );
+            tmp = elems[ i ];
+            elems[ i ] = elems[ idx ];
+            elems[ idx ] = tmp;
+            } 
+              for( var i = 0; i < len; i++ )
+              pn.appendChild( elems[ i ] );
+    }
+        orderElems( 'rep1', 'rep2', 'rep3', 'rep4' );*/
 
-orderElems( 'btn1', 'btn2', 'btn3','btn4' );
+        /*$(".row").html($(".row justify-content-center .btn").sort(function(){
+          return Math.random()-0.5;
+      }));*/
+                
